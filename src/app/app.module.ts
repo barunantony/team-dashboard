c2fe5543c999e1d601e2d279234b4eacac42e763
Arun Antony <arun.antony@knab.nl>
2018-11-14 09:59:32 +0100

    merge resolving conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
 
 import { SlideShowComponent } from 'src/sharedComponents/slide-show/slide-show.component';
 import { ClickableCardsComponent } from 'src/sharedComponents/clickable-cards/clickable-cards.component';
@@ -11,14 +10,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { CurrentSprintComponent } from './components/jira-dashboard-components/current-sprint/current-sprint.component';
 import { TopDelayedStoriesComponent } from './components/jira-dashboard-components/top-delayed-stories/top-delayed-stories.component';
 import { TrafficDashboardComponent } from './components/traffic-dashboard/traffic-dashboard.component';
-=======
-
-import { AppComponent } from './app.component';
-import { CurrentSprintComponent } from './components/jira-dashboard-components/current-sprint/current-sprint.component';
-import { TopDelayedStoriesComponent } from './components/jira-dashboard-components/top-delayed-stories/top-delayed-stories.component';
-import { TrafficDashboardComponent } from './components/traffic-dashboard/traffic-dashboard.component';
-import { ClickableCardsComponent } from 'src/sharedComponents/clickable-cards/clickable-cards.component';
->>>>>>> fb12ba388ec8366398e28f6b1e8b6090eb04530f
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
@@ -26,11 +17,8 @@ import { AppRoutingModule } from './app-routing.module';
     AppComponent,
     ClickableCardsComponent,
     CurrentSprintComponent,
-<<<<<<< HEAD
     DashboardComponent,
     SlideShowComponent,
-=======
->>>>>>> fb12ba388ec8366398e28f6b1e8b6090eb04530f
     TopDelayedStoriesComponent,
     TrafficDashboardComponent
   ],
