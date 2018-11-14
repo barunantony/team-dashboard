5036b8080d90f7b48678ce969bbc68db0c4a7a9f
Arun Antony <arun.antony@knab.nl>
2018-11-14 10:03:09 +0100

    moved the dashboard to routing component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from "@angular/router";
 
 import { CurrentSprintComponent } from "./components/jira-dashboard-components/current-sprint/current-sprint.component";
 import { TopDelayedStoriesComponent } from "./components/jira-dashboard-components/top-delayed-stories/top-delayed-stories.component";
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
 
 const appRoutes: Routes = [
+    { path: '', component: DashboardComponent },
     { path: 'active-sprint', component: CurrentSprintComponent },
     { path: 'delayed-stories', component: TopDelayedStoriesComponent },
 ];
